93e05d8eb07d59a7517cd25f4355b6022554c7bd
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-10-20 10:27:39 -0300

    Setup eslint/prettier

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
